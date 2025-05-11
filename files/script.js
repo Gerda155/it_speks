d424297275c6d4ff8a880fd94ffb95bed1717ba5
Gerda Fedotova <gerda.fedotova.sk@lvt.lv>
2025-05-11 17:54:34 +0300

    commit

---
+++ files/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('.sidebar-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const item = header.parentElement;
+        const allItems = document.querySelectorAll('.sidebar-item');
+
+        allItems.forEach(other => {
+            if (other !== item) other.classList.remove('active');
+        });
+
+        item.classList.toggle('active');
+    });
+}); // вкладки
